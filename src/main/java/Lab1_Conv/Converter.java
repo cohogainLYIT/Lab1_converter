0b30d4fc3af0714da27c2234766b29fda746d3fc
L00169829 <l00169829@student.lyit.ie>
2021-10-30 14:16:21 +0100

    added fahrenheit and celcius converter

--- src/main/java/Lab1_Conv/Converter.java
+++ src/main/java/Lab1_Conv/Converter.java
@@ -13,6 +13,16 @@ public class Converter {
 	 * @param args
 	 */
 	
+	//Celcius to Fahrenheit conversion
+	public static int celc_to_fahr(int celc) {
+		return (celc * 9/5) + 32;
+	}
+	
+	//Fahrenheit to Celcius conversion
+	public static int fahr_to_celc(int fahr) {
+		return (fahr - 32) * 5/9;
+	}
+	
 	//miles to kilometres conversion
 	public static int miles_to_kms(int miles) {
 		return (miles/5)*8;
@@ -22,6 +32,7 @@ public class Converter {
 	public static int kms_to_miles(int kms) {
 		return (kms/8)*5;
 	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
--- src/test/java/Lab1_Conv/Converter_Test.java
+++ src/test/java/Lab1_Conv/Converter_Test.java
@@ -13,12 +13,28 @@ import org.junit.Test;
  *
  */
 public class Converter_Test {
+	
+	/**
+	 * Test method for {@link Lab1_Conv.Converter#miles_to_kms(int)}.
+	 */
+	@Test
+	public void test_fahr_to_celc() {
+		assertEquals(32135,Converter.fahr_to_celc(57875));
+	}
+	
+	/**
+	 * Test method for {@link Lab1_Conv.Converter#miles_to_kms(int)}.
+	 */
+	@Test
+	public void test_celc_to_fahr() {
+		assertEquals(98,Converter.celc_to_fahr(37));
+	}
 
 	/**
 	 * Test method for {@link Lab1_Conv.Converter#miles_to_kms(int)}.
 	 */
 	@Test
-	public void testMiles_to_kms() {
+	public void test_miles_to_kms() {
 		assertEquals(16,Converter.miles_to_kms(10));
 	}
 
@@ -26,7 +42,7 @@ public class Converter_Test {
 	 * Test method for {@link Lab1_Conv.Converter#kms_to_miles(int)}.
 	 */
 	@Test
-	public void testKms_to_miles() {
+	public void test_kms_to_miles() {
 		assertEquals(20, Converter.kms_to_miles(32));
 	}
 
@@ -34,7 +50,7 @@ public class Converter_Test {
 	 * Test method for {@link Lab1_Conv.Converter#main(java.lang.String[])}.
 	 */
 	@Test
-	public void testMain() {
+	public void test_main() {
 		equals("Converter!!");
 	}
 
