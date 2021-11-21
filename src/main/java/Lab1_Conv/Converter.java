5410c3d7b7c5ff307a822cb4f0fb38903275a11e
L00169829 <l00169829@student.lyit.ie>
2021-11-21 15:57:23 +0000

    Added exit program function

--- src/main/java/Lab1_Conv/Converter.java
+++ src/main/java/Lab1_Conv/Converter.java
@@ -35,33 +35,38 @@ public class Converter {
 	}
 	
 	public static void main(String[] args) {
-
-		Scanner sc = new Scanner(System.in);
-		
-		System.out.println("Select what option you want to use:");
-		System.out.println("A:  Fahrenheit to Celcius");
-		System.out.println("B:  Celcius to Fahrenheit");
-		System.out.println("C:  Miles to Kilometers");
-		System.out.println("D:  Kilometers to Miles\n");
-		String option_str = sc.next();
-		char option = option_str.charAt(0);
-		try {
-			if(option == 'A') {
-				System.out.println("\nEnter Fahrenheit values:");
-				System.out.println("\nCelcius = " + fahr_to_celc(sc.nextInt()));
-			}else if(option == 'B') {
-				System.out.println("\nEnter Celcius values:");
-				System.out.println("\nFahrenheit = " + celc_to_fahr(sc.nextInt()));
-			}else if(option == 'C') {
-				System.out.println("\nEnter Miles values:");
-				System.out.println("\nKilometers = " + miles_to_kms(sc.nextInt()));
-			}else if(option == 'D') {
-				System.out.println("\nEnter Kilometers values:");
-				System.out.println("\nMiles = " + kms_to_miles(sc.nextInt()));
+		while(true) {
+			Scanner sc = new Scanner(System.in);
+			
+			System.out.println("Select what option you want to use:");
+			System.out.println("A:  Fahrenheit to Celcius");
+			System.out.println("B:  Celcius to Fahrenheit");
+			System.out.println("C:  Miles to Kilometers");
+			System.out.println("D:  Kilometers to Miles\n");
+			System.out.println("E:  Exit");
+			String option_str = sc.next();
+			char option = option_str.charAt(0);
+			try {
+				if(option == 'A') {
+					System.out.println("\nEnter Fahrenheit value:");
+					System.out.println("\nCelsius = " + fahr_to_celc(sc.nextInt()));
+				}else if(option == 'B') {
+					System.out.println("\nEnter Celcius value:");
+					System.out.println("\nFahrenheit = " + celc_to_fahr(sc.nextInt()));
+				}else if(option == 'C') {
+					System.out.println("\nEnter Miles value:");
+					System.out.println("\nKilometers = " + miles_to_kms(sc.nextInt()));
+				}else if(option == 'D') {
+					System.out.println("\nEnter Kilometers value:");
+					System.out.println("\nMiles = " + kms_to_miles(sc.nextInt()));
+				}
+				else if(option == 'E') {
+					break;
+				}
+			}catch (Exception e) {
+					System.out.println("\nThis is not an option, bye!");
+					System.exit(1);
 			}
-		}catch (Exception e) {
-				System.out.println("\nThis is not an option, bye!");
-				System.exit(1);
 		}
 
 	}
